5f1526b0f469ae8f3d942e64efeaac7195702e03
Hein <hein@bitechsystems.co.za>
2025-04-29 14:12:00 +0200

    Fixed RegisterModel variable shadowed

--- pkg/resolvespec/utils.go
+++ pkg/resolvespec/utils.go
@@ -68,8 +68,8 @@ func (h *APIHandler) getModelForEntity(schema, name string) (interface{}, error)
 
 func (h *APIHandler) RegisterModel(schema, name string, model interface{}) error {
 	fullname := fmt.Sprintf("%s.%s", schema, name)
-	model, err := models.GetModelByName(fullname)
-	if model != nil && err != nil {
+	oldModel, err := models.GetModelByName(fullname)
+	if oldModel != nil && err != nil {
 		return fmt.Errorf("model %s already exists", fullname)
 	}
 	err = models.RegisterModel(model, fullname)
